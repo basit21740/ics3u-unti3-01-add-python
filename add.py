b23ac3fd667b4ba6988f6e086dad08884dfae621
abdul <abdul.butt@mths.ca>
2021-11-18 17:01:36 +0000

    added addition program

---
+++ add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Created by: Abdul Basit
+# Created on: Nov 2021
+# This program adds two numbers user inputs
+
+first = int(input("Enter first number(integer):"))
+second = int(input("Enter second number(integer):"))
+
+print(f"{first} + {second} = {first + second}")
+print("")
+print("Done.")
